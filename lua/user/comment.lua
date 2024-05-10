1136e82997ee8d8e1437d9cb20a2c8220ed0af23
Elliot <mr.elliot.se@gmail.com>
2024-05-10 18:11:33 +0800

    update

--- lua/user/comment.lua
+++ lua/user/comment.lua
@@ -29,7 +29,6 @@ function M.config()
   require('ts_context_commentstring').setup {
     custom_calculation = function(node, language_tree)
       local filename = vim.fn.expand('%:t')
-
       -- print(language_tree:lang())
       if language_tree:lang() == 'html' then
         if filename:match("%.blade.php$") then
