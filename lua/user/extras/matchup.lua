f8814c732701b6980ac198cf1131f584215b61c4
Elliot <mr.elliot.se@gmail.com>
2025-02-05 16:38:10 -0600

    updated

--- lua/user/extras/matchup.lua
+++ lua/user/extras/matchup.lua
@@ -4,8 +4,10 @@ local M = {
 
 function M.config()
   -- vim.g.matchup_enabled = 0
-  vim.g.matchup_matchparen_offscreen = { method = nil }
+  -- vim.g.matchup_matchparen_offscreen = { method = nil }
+  vim.g.matchup_matchparen_offscreen = { method = "popup" }
   vim.g.matchup_matchpref = { html = { nolists = 1 } }
+
 --   vim.cmd [[
 -- augroup matchup_matchparen_disable_ft
 --   autocmd!
@@ -14,6 +16,7 @@ function M.config()
 -- augroup END
 -- ]]
 
+  vim.cmd [[hi MatchParen cterm=none ctermfg=84 gui=none guifg=#F45F5F]]
 end
 
 return M
