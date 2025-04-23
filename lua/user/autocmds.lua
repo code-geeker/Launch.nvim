0da477d295ee1dc2706b4c0fffcfe54474348166
Elliot <mr.elliot>
2025-04-22 19:53:08 -0500

    updated

--- lua/user/autocmds.lua
+++ lua/user/autocmds.lua
@@ -1,8 +1,8 @@
-vim.api.nvim_create_autocmd({ "BufEnter" }, {
+--[[ vim.api.nvim_create_autocmd({ "BufEnter" }, {
   callback = function()
     vim.cmd "syntax sync fromstart"
   end,
-})
+}) ]]
 
 vim.api.nvim_create_autocmd({ "BufWinEnter" }, {
   callback = function()
